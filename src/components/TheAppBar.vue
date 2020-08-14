69f419661a911817220d85991091259fcb6a91cb
GRasul99 <rasulganiyev1999@gmail.com>
2020-08-14 20:56:00 +0500

    Modified TheAppBar for auth

--- src/components/TheAppBar.vue
+++ src/components/TheAppBar.vue
@@ -2,16 +2,29 @@
   <div class="app-bar -shadow">
     <img src="@/assets/logo.png" alt="Logo" class="app-bar__img" />
     <div class="app-bar-auth">
-      <router-link :to="{ name: 'login' }" class="app-bar-auth__login"
-        >Логин</router-link
-      >
-      <router-link :to="{ name: 'register' }" class="app-bar-auth__register"
-        >Регистрация</router-link
-      >
+      <router-link v-if="!user" :to="{ name: 'login' }" class="app-bar-auth__login">Логин</router-link>
+      <router-link
+        v-if="!user"
+        :to="{ name: 'register' }"
+        class="app-bar-auth__register"
+      >Регистрация</router-link>
+      <BaseButton v-if="user" @click="logout">Logout</BaseButton>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'TheAppBar',
+  computed: {
+    user() {
+      return this.$store.state.auth.authUser
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('auth/logout')
+    }
+  }
+}
 </script>
