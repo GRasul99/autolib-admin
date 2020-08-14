8fd378062bdbf49160ead62ad424006028c57b00
GRasul99 <rasulganiyev1999@gmail.com>
2020-08-14 21:20:22 +0500

    Added search for BookList.vue

--- src/views/BookList.vue
+++ src/views/BookList.vue
@@ -1,7 +1,24 @@
 <template>
-  <section class="home"></section>
+  <section class="home section">
+    <form class="form form-search -shadow">
+      <div class="form-content-input">
+        <BaseIcon name="search" />
+        <BaseInput type="serch" placeholder="Поиск..." v-model="search" />
+      </div>
+    </form>
+  </section>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'BookList',
+  data() {
+    return {
+      search: ''
+    }
+  }
+}
 </script>
+
+<style lang="scss">
+</style>
