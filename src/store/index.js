51aca763eecd4d4aca7c484db079d138bd4d5d82
GRasul99 <rasulganiyev1999@gmail.com>
2020-08-15 08:57:11 +0500

    Added functionality for OrdersList.vue

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import * as book from '@/store/modules/book'
 import * as auth from '@/store/modules/auth'
-import * as order from '@/store/modules/auth'
-import * as user from '@/store/modules/auth'
+import * as order from '@/store/modules/order'
+import * as user from '@/store/modules/user'
 
 Vue.use(Vuex)
 
--- src/views/OrdersList.vue
+++ src/views/OrdersList.vue
@@ -14,17 +14,83 @@
       </thead>
       <hr />
       <tbody>
-        <tr>
-          <td>body</td>
-        </tr>
+        <!--        <tr v-for="(searchedOrder, index) in searchedOrders" :key="index">-->
+        <!--          <td>-->
+        <!--            <div class="order-id">{{ index + 1 }}</div>-->
+        <!--          </td>-->
+        <!--          <td>-->
+        <!--            <div class="user-avatar">-->
+        <!--              <img-->
+        <!--                :src="-->
+        <!--                  `https://libro.pythonanywhere.com/` + searchedOrder.Аватар-->
+        <!--                "-->
+        <!--                alt="Avatar"-->
+        <!--              />-->
+        <!--            </div>-->
+        <!--          </td>-->
+        <!--          <td>-->
+        <!--            <div class="user-info">-->
+        <!--              <div>-->
+        <!--                {{ $t('orders.body.id') }} :-->
+        <!--                <span>{{ searchedOrder.qr_number }}</span>-->
+        <!--              </div>-->
+        <!--              <div>-->
+        <!--                {{ $t('orders.body.name') }}:-->
+        <!--                <span>{{ searchedOrder.full_name }}</span>-->
+        <!--              </div>-->
+        <!--              <div>-->
+        <!--                {{ $t('orders.body.faculty') }}:-->
+        <!--                <span>{{ searchedOrder.faculty }}</span>-->
+        <!--              </div>-->
+        <!--              <div>-->
+        <!--                {{ $t('orders.body.course') }}:-->
+        <!--                <span>{{ searchedOrder.course }}</span>-->
+        <!--              </div>-->
+        <!--            </div>-->
+        <!--          </td>-->
+        <!--          <td>-->
+        <!--            <div class="order-books">-->
+        <!--              <img-->
+        <!--                :src="`https://libro.pythonanywhere.com/` + searchedOrder.img"-->
+        <!--                alt="Order books"-->
+        <!--              />-->
+        <!--            </div>-->
+        <!--          </td>-->
+        <!--          <td>-->
+        <!--            <div class="order-name">{{ searchedOrder.title }}</div>-->
+        <!--          </td>-->
+        <!--          <td>-->
+        <!--            <div class="order-book-isbn">{{ searchedOrder.isbn }}</div>-->
+        <!--          </td>-->
+        <!--          <td>-->
+        <!--            <div class="order-date_pub">{{ searchedOrder.date_pub }}</div>-->
+        <!--          </td>-->
+        <!--          <td>-->
+        <!--            <div class="order-author">{{ searchedOrder.author }}</div>-->
+        <!--          </td>-->
+        <!--          <td>-->
+        <!--            <div class="order-udc">{{ searchedOrder.udc }}</div>-->
+        <!--          </td>-->
+        <!--          <td>-->
+        <!--            <div class="order-remained_time">-->
+        <!--              {{ searchedOrder.remained_time }}-->
+        <!--            </div>-->
+        <!--          </td>-->
+        <!--          <td>-->
+        <!--            <div></div>-->
+        <!--          </td>-->
+        <!--        </tr>-->
       </tbody>
     </table>
   </section>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import store from '@/store'
+
 export default {
-  name: 'Oders',
+  name: 'Orders',
   data() {
     return {
       headers: [
@@ -39,8 +105,60 @@ export default {
         { id: 8, name: 'Оставщиейся время' },
         { id: 9, name: 'Забрал' }
       ],
-      search: ''
+      search: '',
+      timeout: null
     }
+  },
+  computed: {
+    ...mapState(['book', 'books', 'user', 'users', 'order', 'orders']),
+    filledOrders() {
+      return this.order.orders.map(order => {
+        const book = this.book.books.find(book => order.book_id === book.id)
+        const user = this.user.users.find(user => order.user === user.id)
+        const remained_time =
+          ((Math.round(Date.parse(order.time_of_order) - Date.now()) %
+            86400000) %
+            3600000) /
+          60000
+        return { ...order, ...book, ...user, remained_time }
+      })
+    },
+    searchedOrders() {
+      return this.filledOrders.filter(data => {
+        return data.full_name.toLowerCase().match(this.search.toLowerCase())
+      })
+    }
+  },
+  mounted() {
+    this.timeout = setInterval(
+      () => this.$store.dispatch('book/fetchBooks'),
+      5000
+    )
+    this.timeout = setInterval(
+      () => this.$store.dispatch('user/fetchUsers'),
+      5000
+    )
+    this.timeout = setInterval(
+      () => this.$store.dispatch('order/fetchOrders'),
+      5000
+    )
+  },
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    store.dispatch('book/fetchBooks')
+    store.dispatch('user/fetchUsers')
+    store.dispatch('order/fetchOrders').then(() => {
+      next()
+    })
+  },
+  beforeRouteUpdate(routeTo, routeFrom, next) {
+    store.dispatch('book/fetchBooks')
+    store.dispatch('user/fetchUsers')
+    store.dispatch('order/fetchOrders').then(() => {
+      next()
+    })
+  },
+  beforeDestroy() {
+    clearTimeout(this.timeout)
   }
 }
 </script>
